e05b40fe5c14fb571ab390638f5a24d55275438f
Paul Krisadaolarn <pauly.k@gmail.com>
2023-12-18 14:24:28 +0700

    working

--- src/lib/components/ScanQrV1.svelte
+++ src/lib/components/ScanQrV1.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
-    
+	import { Html5QrcodeScanner, type Html5QrcodeResult } from 'html5-qrcode';
+	import { onMount } from 'svelte';
+
+	let qrScanner: Html5QrcodeScanner;
+
+	let barcodes = new Set<string>();
+
+	function onScanSuccess(decodedText: string, decodedResult: Html5QrcodeResult) {
+		console.log(`Code matched = ${decodedText}`, decodedResult);
+		barcodes = new Set([...barcodes, decodedText]);
+	}
+
+	function onScanFailure(error: any) {
+		console.warn(`Code scan error = ${error.message}`, error);
+	}
+
+	onMount(() => {
+		qrScanner = new Html5QrcodeScanner('render', { fps: 10, qrbox: 250 }, false);
+		qrScanner.render(onScanSuccess, onScanFailure);
+	});
 </script>
 
-<div id="render">
+<div id="render"></div>
 
-</div>
+<h2>Result</h2>
+<ul>
+	{#each barcodes as barcode}
+		<li>{barcode}</li>
+	{/each}
+</ul>
 
 <style>
-
-    #render {
-        height: 250px;
-        width: 250px;
-        border: 1px solid #444;
-    }
+	#render {
+		height: 250px;
+		width: 600px;
+		border: 1px solid #444;
+        overflow-y: clip;
+	}
 </style>
