9d023f3f19a877a8848deb419514423e476d466e
Paul Krisadaolarn <pauly.k@gmail.com>
2023-12-18 14:11:17 +0700

    initial

---
+++ src/lib/components/ScanQrV1.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    
+</script>
+
+<div id="render">
+
+</div>
+
+<style>
+
+    #render {
+        height: 250px;
+        width: 250px;
+        border: 1px solid #444;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import ScanQrV1 from '$lib/components/ScanQrV1.svelte';
+import '../app.css';
+</script>
+
+<main>
+	<h1>SCAN QrCode POC</h1>
+    <ScanQrV1 />
+</main>
+
+<style>
+	h1 {
+		text-align: center;
+	}
+</style>
